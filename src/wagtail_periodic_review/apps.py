c2607b850a1b98f97a8895a8637e3845ac19e09c
zerolab <dan@zerolab.org>
2023-02-10 20:10:16 +0000

    Add missing app config
    something, something BigAutoField

---
+++ src/wagtail_periodic_review/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WagtailPeriodicReviewConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    label = "wagtail_periodic_review"
+    name = "wagtail_periodic_review"
+    verbose_name = "Wagtail Periodic Review"
