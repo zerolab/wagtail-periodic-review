aebeced8997df21950d1f41097b599a8cb1a4bbf
zerolab <dan@zerolab.org>
2023-08-11 16:58:25 +0100

    Handle `UserPagePermissionsProxy` deprecation in Wagtail 5.1

--- src/wagtail_periodic_review/views.py
+++ src/wagtail_periodic_review/views.py
@@ -1,10 +1,16 @@
 from django.core.exceptions import FieldError
 from django.utils.translation import gettext as _
+from wagtail import VERSION as WAGTAIL_VERSION
 from wagtail.admin.views.reports import ReportView
-from wagtail.models import UserPagePermissionsProxy
 
 from .filters import PeriodicReviewFilterSet
 from .utils import add_review_date_annotations, filter_across_subtypes
+
+
+if WAGTAIL_VERSION >= (5, 1):
+    from wagtail.permission_policies.pages import PagePermissionPolicy
+else:
+    from wagtail.models import UserPagePermissionsProxy
 
 
 class PeriodicReviewContentReport(ReportView):
@@ -13,9 +19,16 @@ class PeriodicReviewContentReport(ReportView):
     template_name = "reports/periodic_review_report.html"
     filterset_class = PeriodicReviewFilterSet
 
+    def _get_editable_pages(self):
+        if WAGTAIL_VERSION >= (5, 1):
+            return PagePermissionPolicy().instances_user_has_permission_for(
+                self.request.user, "change"
+            )
+        return UserPagePermissionsProxy(self.request.user).editable_pages()
+
     def get_queryset(self):
         queryset = filter_across_subtypes(
-            UserPagePermissionsProxy(self.request.user).editable_pages(),
+            self._get_editable_pages(),
             last_review_date__isnull=False,
         )
         try:
