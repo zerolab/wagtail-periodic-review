5ccb2f58b89026adc88efe2c1d0ef865f58ec9d9
Victor Miti <victor.miti@torchbox.com>
2024-08-20 18:32:14 +0100

    Update report views for Wagtail 6.2 compatibility
    https://docs.wagtail.org/en/latest/releases/6.2.html#changes-to-report-views-with-the-new-universal-listings-ui

--- src/wagtail_periodic_review/views.py
+++ src/wagtail_periodic_review/views.py
@@ -1,14 +1,42 @@
 from django.core.exceptions import FieldError
 from django.utils.translation import gettext as _
+from wagtail import VERSION as WAGTAIL_VERSION
 from wagtail.admin.views.reports import ReportView
 from wagtail.permission_policies.pages import PagePermissionPolicy
 
 from .filters import PeriodicReviewFilterSet
 from .utils import add_review_date_annotations, filter_across_subtypes
 
+def _adapt_wagtail_report_attributes(cls):
+    """
+    A class decorator that adapts ReportView-derived classes for compatibility
+    with multiple versions of Wagtail by conditionally assigning attributes
+    based on the Wagtail version. This includes setting appropriate titles,
+    and adjusting template names and URL names for AJAX support since Wagtail 6.2.
+    Attributes adjusted:
+    - `title` or `page_title` based on Wagtail version for the display name of the report.
+    - For Wagtail 6.2 and above, additional attributes like `results_template_name`,
+      `index_results_url_name`, and `index_url_name` are set to support AJAX updates
+      and utilize the `wagtail.admin.ui.tables` framework.
+    """
+    if WAGTAIL_VERSION < (6, 2):
+        cls.title = _("Periodic review content")
+    else:
+        # The `title` attr was **changed** to `page_title` in Wagtail 6.2
+        cls.page_title = _("Periodic review content")
+        # The `results_template_name` attr was **added** in Wagtail 6.2
+        # to support updating the listing via AJAX upon filtering and
+        # to allow the use of the `wagtail.admin.ui.tables` framework.
+        cls.results_template_name = "reports/periodic_review_report_results.html"
+        # The `index_results_url_name` attr was **added** in Wagtail 6.2
+        # to support updating the listing via AJAX upon filtering.
+        cls.index_results_url_name = "wagtail_periodic_review_report_results"
+        cls.index_url_name = "wagtail_periodic_review_report"
+    return cls
 
+
+@_adapt_wagtail_report_attributes
 class PeriodicReviewContentReport(ReportView):
-    title = _("Periodic review content")
     header_icon = "wpr-calendar-stats"
     template_name = "reports/periodic_review_report.html"
     filterset_class = PeriodicReviewFilterSet
--- src/wagtail_periodic_review/wagtail_hooks.py
+++ src/wagtail_periodic_review/wagtail_hooks.py
@@ -2,6 +2,7 @@ from typing import Any, Mapping
 
 from django.urls import path, reverse
 from django.utils.translation import gettext as _
+from wagtail import VERSION as WAGTAIL_VERSION
 from wagtail import hooks
 from wagtail.admin.menu import MenuItem
 from wagtail.admin.ui.components import Component
@@ -83,13 +84,25 @@ def register_report_menu_item():
 
 @hooks.register("register_admin_urls")
 def register_report_url():
-    return [
+    urls = [
         path(
             "reports/periodic-review/",
             PeriodicReviewContentReport.as_view(),
             name="wagtail_periodic_review_report",
         )
     ]
+
+    if WAGTAIL_VERSION >= (6, 2):
+        # Add a results-only view to add support for AJAX-based filtering
+        urls.append(
+            path(
+                "reports/periodic-review/results/",
+                PeriodicReviewContentReport.as_view(results_only=True),
+                name="wagtail_periodic_review_report_results",
+            ),
+        )
+
+    return urls
 
 
 @hooks.register("register_icons")
