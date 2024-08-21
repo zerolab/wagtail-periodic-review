cf7833f13cd6abc0aa3a041fc3df2a7c142c0175
Victor Miti <victor.miti@torchbox.com>
2024-08-21 16:02:01 +0100

    Fix broken tox interactive test

--- tests/manage.py
+++ tests/manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+from pathlib import Path
+
+
+# Add the project directory to the Python system path to enable importing of project modules
+sys.path.append(Path(__file__).resolve().parent.parent.__str__())
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
